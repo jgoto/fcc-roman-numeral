b2ca2e41a5c67143996b1090cf866cfd897c7161
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2024-08-21 08:23:05 -0400

    Added a function to the button to validate the input with javascript and run the convertRomanNumeral function on valid inputs and display it on the page.

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+const numberInput = document.getElementById("number");
+const checkBtn = document.getElementById("convert-btn");
+const output = document.getElementById("output");
+
+checkBtn.addEventListener("click", ()=>{
+    if(number.value==="")
+    {
+        alert("Please enter a valid number")
+    }
+    else if(number.value < 0)
+    {
+        alert("Please enter a number greater than or equal to 1");
+        number.value = "";
+    }
+    else if(number.value > 3999)
+    {
+        alert("Please enter a number less than or equal to 3999")
+        number.value = "";
+    }
+    else
+    {
+        output.innerText = convertRomanNumeral(number.value);
+    }
+})
+
+
+
 const romanNumeralMap = new Map([
     [1000, "M"],
     [900, "CM"],
